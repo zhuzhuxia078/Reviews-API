c5f57641bf61bbf1b93c1045929823b13e7601a9
Qinyu Zhu <zz635186056@gmail.com>
2021-09-30 22:44:09 -0700

    init GET reviews API

--- server/db/database.js
+++ server/db/database.js
@@ -1,12 +1,4 @@
-const { Pool } = require('pg');
 
-const pool = new Pool({
-  user: 'zhuqinyu',
-  host: 'localhost',
-  database: 'review_service',
-  password: '',
-  port: 5432,
-});
 
 // pool.query('SELECT NOW()', (err, res) => {
 //   console.log('test pool.query');
@@ -31,75 +23,7 @@ const pool = new Pool({
 //   client.end();
 // });
 
-pool.on('error', (err, client) => {
-  console.error('Unexpected error on idle client', err);
-  process.exit(-1);
-});
 
-const getReview = 'SELECT * FROM reviews LIMIT 10';
-const getPhoto = 'SELECT * FROM reviews_photos LIMIT 10';
-const getValue = 'SELECT * FROM characteristic_reviews LIMIT 10';
-const getChara = 'SELECT * FROM characteristics LIMIT 10';
-
-pool
-  .connect()
-  .then((client) => {
-    return client
-      .query(getReview)
-      .then((res) => {
-        client.release()
-        console.log(res.rows)
-      })
-      .catch(err => {
-        client.release()
-        console.log(err.stack)
-      })
-  });
-
-  pool
-  .connect()
-  .then((client) => {
-    return client
-      .query(getPhoto)
-      .then((res) => {
-        client.release()
-        console.log(res.rows)
-      })
-      .catch(err => {
-        client.release()
-        console.log(err.stack)
-      })
-  });
-
-  pool
-  .connect()
-  .then((client) => {
-    return client
-      .query(getChara)
-      .then((res) => {
-        client.release()
-        console.log(res.rows)
-      })
-      .catch(err => {
-        client.release()
-        console.log(err.stack)
-      })
-  });
-
-  pool
-  .connect()
-  .then((client) => {
-    return client
-      .query(getValue)
-      .then((res) => {
-        client.release()
-        console.log(res.rows)
-      })
-      .catch(err => {
-        client.release()
-        console.log(err.stack)
-      })
-  });
 // async function getReview() {
 //   console.log('test in db');
 //   // const review = 'SELECT * FROM reviews LIMIT 5';
@@ -113,4 +37,4 @@ pool
 
 
 
-module.exports = { getReview };
+// module.exports = pool;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
-const db = require('./db/database.js');
+const { Pool } = require('pg');
+
+
 
 const app = express();
 
 const port = process.env.PORT || 2000;
+
+const pool = new Pool({
+  user: 'zhuqinyu',
+  host: 'localhost',
+  database: 'review_service',
+  password: '',
+  port: 5432,
+});
 
 app.use(express.json());
 
@@ -11,7 +21,80 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
 
+const getReview = 'SELECT * FROM reviews LIMIT 10';
+const getPhoto = 'SELECT * FROM reviews_photos LIMIT 10';
+const getValue = 'SELECT * FROM characteristic_reviews LIMIT 10';
+const getChara = 'SELECT * FROM characteristics LIMIT 10';
+
+
+app.get('/reviews', (req, res) => {
+  const getReview = 'SELECT * FROM reviews LIMIT 10';
+  pool
+  .connect()
+  .then((client) => {
+    return client
+      .query(getReview)
+      .then((res) => {
+        client.release()
+        console.log(res.rows)
+      })
+      .catch(err => {
+        client.release()
+        console.log(err.stack)
+      })
+  });
+})
+
+
+  pool
+  .connect()
+  .then((client) => {
+    return client
+      .query(getChara)
+      .then((res) => {
+        client.release()
+        console.log(res.rows)
+      })
+      .catch(err => {
+        client.release()
+        console.log(err.stack)
+      })
+  });
+
+  pool
+  .connect()
+  .then((client) => {
+    return client
+      .query(getPhoto)
+      .then((res) => {
+        client.release()
+        console.log(res.rows)
+      })
+      .catch(err => {
+        client.release()
+        console.log(err.stack)
+      })
+  });
+
+  pool
+  .connect()
+  .then((client) => {
+    return client
+      .query(getValue)
+      .then((res) => {
+        client.release()
+        console.log(res.rows)
+      })
+      .catch(err => {
+        client.release()
+        console.log(err.stack)
+      })
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
